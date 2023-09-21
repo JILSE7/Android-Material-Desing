b8f4ea49a98ae4a8285ff9c333a6ec07911f519b
JILSE7 <saidnnnn@gmail.com>
2023-09-20 22:46:56 -0600

    Material part 2

--- app/src/main/java/org/bedu/a03material/MainActivity.kt
+++ app/src/main/java/org/bedu/a03material/MainActivity.kt
@@ -1,7 +1,12 @@
 package org.bedu.a03material
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.snackbar.Snackbar
 import org.bedu.a03material.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +19,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        binding.fab.setOnClickListener {
+            if (binding.bottomAppBar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER) {
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+            } else {
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+            }
+        }
+
+        binding.bottomAppBar.setNavigationOnClickListener{
+            Snackbar.make(binding.root, R.string.message_sucess,Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .show()
+        }
+
+        binding.btnSkip.setOnClickListener {
+            binding.card1.visibility = View.GONE
+        }
+
+        binding.btnBuy.setOnClickListener {
+            Snackbar.make(it, R.string.card_buy, Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .setAction(R.string.card_buy) {
+                    Toast.makeText(this, R.string.history, Toast.LENGTH_LONG).show()
+                }
+                .show()
+        }
     }
 }
