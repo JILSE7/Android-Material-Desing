10a4b968892b0a3a499bb9786c963d2bbc92fa1d
JILSE7 <saidnnnn@gmail.com>
2023-10-02 20:26:34 -0600

    Chip material desing

--- app/src/main/java/org/bedu/a03material/MainActivity.kt
+++ app/src/main/java/org/bedu/a03material/MainActivity.kt
@@ -1,10 +1,15 @@
 package org.bedu.a03material
 
-import android.opengl.Visibility
+import android.graphics.Color
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.view.View
+import android.webkit.URLUtil
 import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.snackbar.Snackbar
 import org.bedu.a03material.databinding.ActivityMainBinding
@@ -45,5 +50,56 @@ class MainActivity : AppCompatActivity() {
                 }
                 .show()
         }
+        loadImg()
+
+        binding.cbEnablePass.setOnClickListener {
+            binding.edPass.isEnabled = !binding.edPass.isEnabled
+        }
+
+        binding.edUrl.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            val edText = binding.edUrl.text.toString()
+            if (!hasFocus && edText.isNotEmpty()) {
+                loadImg(binding.edUrl.text.toString())
+                return@OnFocusChangeListener
+            }
+            if (!hasFocus && edText.isEmpty() || !hasFocus && !URLUtil.isValidUrl(edText)) {
+                binding.edUrl.error = "Sin url"
+            }
+        }
+
+
+        binding.toggleBtn.addOnButtonCheckedListener { _, checkedId, _ ->
+            binding.root.setBackgroundColor(
+                when(checkedId){
+                    R.id.btn_red -> Color.RED
+                    R.id.btn_blue -> Color.BLUE
+                    else -> Color.YELLOW
+
+                }
+            )
+
+        }
+
+        binding.swFab.setOnCheckedChangeListener { V, isChecked ->
+            if (isChecked) {
+                binding.fab.show()
+            }else {
+                binding.fab.hide()
+            }
+        }
+
+        binding.slider.addOnChangeListener { slider, value, fromUser ->
+
+            binding.textView2.text = "Vol $value"
+        }
     }
+
+    private fun loadImg(imgUrl: String = "https://cdn2.excelsior.com.mx/media/styles/image800x600/public/pictures/2021/06/09/2592367.jpg") {
+        Glide.with(this)
+            .load(imgUrl)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(binding.imgCover)
+    }
+
 }
